35a3e2c7824ca0895d153ded9d990b492fb59139
Erica M Herrera Huaman <eherrer6@terpmail.umd.edu>
2025-02-26 19:06:40 +0000

    ex 5

--- UserInput-ex5.cpp
+++ UserInput-ex5.cpp
@@ -2,11 +2,25 @@
 #include <iostream>
 using namespace std;
 
+int factorial (int a) {
+  int i; int ans;
+  ans = 1;
+  for (i = 1; i <= a; i++) {
+      ans = ans * i;
+      //cout << "loop count is " << i << endl;
+      //cout << "factorial of " << i << " is " << ans << endl;
+  }
+  return ans;
+}
+
 int main() {
       // Variable to store data given by cin
-    int a;
+    int inputs; int answers;
     // Take input using cin
-    cin >> a;
-    cout << a;
+    cout << "Please enter an integer value: \n";
+    cin >> inputs; 
+    
+    answers = factorial(inputs);
+    cout << "The value you entered is " << inputs << " and its factorial is " << answers << "!" << endl;
     return 0;
 }
